37bbf7ae1a54153ce06eed3b8eaf3a22bf261184
subtubes-io <edgar@subtubes.io>
2024-10-09 05:58:37 -0700

    Improve sql for readability

--- drizzle/0000_plain_terror.sql
+++ drizzle/0000_plain_terror.sql
@@ -1,7 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS events;
-CREATE TABLE
-IF
-	NOT EXISTS "events" (
+CREATE TABLE IF NOT EXISTS "events" (
 		"id" serial PRIMARY KEY NOT NULL,
 		"event_type" TEXT NOT NULL,
 		"user_id" INTEGER,
@@ -10,38 +8,23 @@ IF
 		"created_at" TIMESTAMPTZ DEFAULT now( ) NOT NULL,
 		"updated_at" TIMESTAMPTZ DEFAULT now( ) NOT NULL 
 	);
-CREATE TABLE
-IF
-	NOT EXISTS "events_partition" (
-		"id" serial NOT NULL,
-		"event_type" TEXT NOT NULL,
-		"user_id" INTEGER,
-		"system_id" INTEGER,
-		"event_data" JSONB NOT NULL,
-		"created_at" TIMESTAMPTZ DEFAULT now( ) NOT NULL,
-		"updated_at" TIMESTAMPTZ DEFAULT now( ) NOT NULL 
-	) PARTITION BY RANGE ( "created_at" );
-CREATE TABLE "events"."events_partition_2024_07" PARTITION OF "public"."events_partition" FOR
-VALUES
-	
-FROM
-	( '2024-07-01 00:00:00' ) TO ( '2024-08-01 00:00:00' );
-CREATE TABLE "events"."events_partition_2024_08" PARTITION OF "public"."events_partition" FOR
-VALUES
-FROM
-	( '2024-08-01 00:00:00' ) TO ( '2024-09-01 00:00:00' );
-CREATE TABLE "events"."events_partition_2024_09" PARTITION OF "public"."events_partition" FOR
-VALUES
-FROM
-	( '2024-09-01 00:00:00' ) TO ( '2024-10-01 00:00:00' );
-CREATE TABLE "events"."events_partition_2024_10" PARTITION OF "public"."events_partition" FOR
-VALUES
-FROM
-	( '2024-10-01 00:00:00' ) TO ( '2024-11-01 00:00:00' );
+
+-- partition table
+CREATE TABLE IF NOT EXISTS "events_partition" (LIKE events ) PARTITION BY RANGE ( "created_at" );
+
+-- partitions
+CREATE TABLE "events"."events_partition_2024_07" PARTITION OF "public"."events_partition" 
+FOR VALUES FROM ( '2024-07-01 00:00:00' ) TO ( '2024-08-01 00:00:00' );
+
+CREATE TABLE "events"."events_partition_2024_08" PARTITION OF "public"."events_partition" 
+FOR VALUES FROM ( '2024-08-01 00:00:00' ) TO ( '2024-09-01 00:00:00' );
+
+CREATE TABLE "events"."events_partition_2024_09" PARTITION OF "public"."events_partition" 
+FOR VALUES FROM ( '2024-09-01 00:00:00' ) TO ( '2024-10-01 00:00:00' );
+
+CREATE TABLE "events"."events_partition_2024_10" PARTITION OF "public"."events_partition" 
+FOR VALUES FROM ( '2024-10-01 00:00:00' ) TO ( '2024-11-01 00:00:00' );
+
 -- indexes
-CREATE INDEX
-IF
-	NOT EXISTS idx_event_created_at ON events ( created_at );
-CREATE INDEX
-IF
-	NOT EXISTS idx_id_event ON events ( ID );
+CREATE INDEX IF NOT EXISTS idx_event_created_at ON events ( created_at );
+CREATE INDEX IF NOT EXISTS idx_id_event ON events ( ID );
